78fc13c4089f6b2fa052b742cfa9caff2d0b987f
Greg Tucker <2191051t@student.gla.ac.uk>
2017-06-16 01:35:07 +0100

    Still setting up heroku

--- app.py
+++ app.py
@@ -24,4 +24,6 @@ def signup_user(id):
     return render_template('signup.html', id=id)
 
 if __name__ == "__main__":
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
