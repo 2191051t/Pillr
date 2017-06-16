433c92f00b576507cc3ff2347429212a288d7a78
Greg Tucker <2191051t@student.gla.ac.uk>
2017-06-16 01:06:54 +0100

    Setting up heroku

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, url_for, render_template
+from web.py import web
+
+db = web.database(dbn='postgres', db='dbname', user='username', pw='password')
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'pillar.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/signup/<int:id>')
+def signup_user(id):
+    # show signup page and have unique id
+    return render_template('signup.html', id=id)
+
+if __name__ == "__main__":
+    app.run()
