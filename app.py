a3abcf4c33b45222ab052f6f78b7dc7687f87b67
Greg Tucker <2191051t@student.gla.ac.uk>
2017-06-16 01:46:20 +0100

    Still setting up heroku

--- app.py
+++ app.py
@@ -25,5 +25,4 @@ def signup_user(id):
 
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
-    #port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
