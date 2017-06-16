521d3bb72a5e3810596acaaf63fb8842a0516312
“Fraser <2192722g@student.gla.ac.uk>
2017-06-16 06:18:40 +0100

    db

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, url_for, render_template
 import os
 import web
-#db = web.database(dbn='postgres', db='dbname', user='username', pw='password')
 
+def urltodb(url):
+	url = url.replace('//','').replace('@',':').split(':')
+	url[-1] = url[-1][url[-1].index('/')+1:]
+	return web.database(dbn=url[0],db=url[4],user=url[1],pw=url[2],host=url[3])
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL is None:
+	db = urltodb('postgres://gmidudxcequwfh:6b2dc6b5aaaabd99b657660d646000dd3bc31e6d6c4d0762b722a93296431842@ec2-54-228-235-185.eu-west-1.compute.amazonaws.com:5432/dckaen6f7jcmrd')
+else:
+	db = urltodb(DATABASE_URL)
 app = Flask(__name__)
-#app.config.from_object(__name__)
-#app.config.update(dict(
-#    #DATABASE=os.path.join(app.root_path, 'pillar.db'),
-#    SECRET_KEY='development key',
-#    USERNAME='admin',
-#    PASSWORD='default'
-#))
-#app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 @app.route("/")
 def hello():
